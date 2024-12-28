e35224bf927b0565f7c75ec77837edc45594a8df
AnupongPakee <anuphong4407@gmail.com>
2024-12-28 14:07:46 +0700

    update ux/ui home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
+import React from 'react'
 
 import { NavLink } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Hello World !!!</h1>
-      <NavLink to={"/pj-dpu-StressCounselingChatbot/test"}>Go to Test</NavLink>        
     </div>
   )
 }
