8b0ffdee8e7696c1352230d877c94f19e1399920
AnupongPakee <anuphong4407@gmail.com>
2024-12-29 14:33:21 +0700

    update ux/ui home + responsive t0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-
 import { NavLink } from 'react-router-dom'
+
+import STYLE from "./style/Style"
 
 function App() {
 
   return (
-    <div>
+    <div className="container home" style={STYLE.container}>
+      <div className="bg-content" style={STYLE['bg-content']}>
+        <div className="bg-1" style={STYLE['bg-1']}></div>
+        <div className="bg-2" style={STYLE['bg-2']}></div>
+        <div className="bg-3" style={STYLE['bg-3']}></div>
+      </div>
+      <div className="content" style={STYLE.content}>
+        <div className="content-left">
+          <h1 style={STYLE.font_family.en}>DPU Stress Counseling Chatbot</h1>
+          <div className="link">
+            <NavLink to={"/pj-dpu-StressCounselingChatbot/login"} style={STYLE.font_family.en}>Get Start</NavLink>
+          </div>
+        </div>
+      </div>
+      <div className="not-support">
+        <p style={STYLE.font_family.en}>Not Support Screen Size ({window.screen.width}x{window.screen.height})</p>
+      </div>
+      <div className="w-h">
+        <p style={STYLE.font_family.en}>{window.screen.width}x{window.screen.height}</p>
+      </div>
     </div>
   )
 }
---
+++ src/style/Style.jsx
@@ -0,0 +1,70 @@
+const style = {
+    "container": {
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        padding: "1.6rem",
+        backgroundColor: "#E0EAFC",
+        overflow: "hidden"
+    },
+    "content": {
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
+        padding: "1.6rem",
+        width: "100%",
+        height: "100%",
+        padding: "1rem",
+        zIndex: "3"
+    },
+    "font_family":
+    {
+        "en": {
+            fontFamily: "Roboto, sans-serif",
+            fontWeight: "400",
+            letterSpacing: "1px"
+        },
+        "th": {
+            fontFamily: "IBM Plex Sans Thai, sans-serif",
+            fontWeight: "400",
+            letterSpacing: "1px"
+        }
+    },
+    "bg-content": {
+        position: "absolute",
+        top: "0",
+        left: "0",
+        display: "flex",
+        width: "100%",
+        height: "100%"
+    },
+    "bg-1": {
+        position: "relative",
+        minWidth: "640px",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#6F3089",
+        boxShadow: "0.4rem 0 1rem rgba(0, 0, 0, 0.5)",
+        zIndex: "3"
+    },
+    "bg-2": {
+        position: "relative",
+        maxWidth: "320px",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#6F3089",
+        boxShadow: "0.4rem 0 1rem rgba(0, 0, 0, 0.5)",
+        zIndex: "2"
+    }, 
+    "bg-3": {
+        position: "relative",
+        maxWidth: "320px",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#6F3089",
+        boxShadow: "0.4rem 0 1rem rgba(0, 0, 0, 0.5)",
+        zIndex: "1"
+    }
+}
+
+export default style;
